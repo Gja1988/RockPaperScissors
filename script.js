ad2e34604f54f09f910f016bdfb1a4d8cfa12f41
Guillermo <garauz1988@gmail.com>
2023-06-25 17:10:27 -0400

    Finish functions and event listeners to properly complete 5 games of RPS.

--- script.js
+++ script.js
@@ -1,34 +1,112 @@
-//Create a function called getComputerChoice that randomly returns either Paper, Rock, Scissors.
+let playScore = 0;
+let compScore = 0;
+let winningScore = 0;
+let roundNum = 0;
+const compChoices = [`rock`, `scissors`, `paper`];
+
+// Names of buttons to add to event listeners
 const rock = document.querySelector('.rock')
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
+
+// Reset button to restart game
+const reset = document.querySelector('.reset');
+
+// Spans to display total score for both player and computer
+const playerDisplay = document.querySelector('#playerScore');
+const compDisplay = document.querySelector('#compScore');
+const winner = document.querySelector('.header');
+
+// Results to display on the screen after each game
 let results = document.querySelector('.results');
 
+// Listener for player selecting rock as their choice. Also creates a list for previous games. 
 rock.addEventListener('click', () => {
-    results.textContent = playRound('rock', getComputerChoice());
+
+    let result = `${playRound('rock', getComputerChoice())}`;
+    const listGames = document.createElement('li');
+    listGames.textContent = result;
+    results.appendChild(listGames);
+    scoreTracker();
+    declareWinner();
 })
 
+// Listener for player selecting paper as their choice. Also creates a list for previous games. 
 paper.addEventListener('click', () => {
-    results.textContent = playRound('paper', getComputerChoice());
+
+    let result = `${playRound('paper', getComputerChoice())}`;
+    const listGames = document.createElement('li');
+    listGames.textContent = result;
+    results.appendChild(listGames);
+    scoreTracker();
+    declareWinner();
 })
 
+// Listener for player selecting scissors as their choice. Also creates a list for previous games. 
 scissors.addEventListener('click', () => {
-    results.textContent = playRound('scissors', getComputerChoice());
+
+    let result = `${playRound('scissors', getComputerChoice())}`;
+    const listGames = document.createElement('li');
+    listGames.textContent = result;
+    results.appendChild(listGames);
+    scoreTracker();
+    declareWinner();
 })
 
-const choices = [`rock`, `scissors`, `paper`];
-
-function getComputerChoice() {
-
-    const random = Math.floor(Math.random() * choices.length);
-
-    return choices[random];
+// Function to reset the game
+const resetGame = () => {
+    results.innerHTML = '';
+    isGameover = false;
+    playScore = 0;
+    compScore = 0;
+    roundNum = 0;
+    compDisplay.textContent = `Computer score: ${compScore}`;
+    playerDisplay.textContent = `Player score: ${playScore}`;
+    enableButtons();
+    winner.textContent = `G's Paper, Rock, Scissors Game`;
 }
 
-/*Create a function that plays a single round of Rock Paper Scissors. This function takes two
-parameters - the "playerSelection" and the "computerSelection", and then return a string that
-declares the winner of that round
-*/
+const disableButtons = () => {
+    rock.disabled = true;
+    scissors.disabled = true;
+    paper.disabled = true;
+}
+
+const enableButtons = () => {
+    rock.disabled = false;
+    scissors.disabled = false;
+    paper.disabled = false;
+}
+
+const scoreTracker = () => {
+    if (playScore >= 5 || compScore >= 5) {
+        disableButtons();
+    }
+}
+
+const declareWinner = () => {
+    if (playScore >= 5) {
+        winner.textContent = `Player wins! Total rounds played: ${roundNum}`;
+    }
+
+    if (compScore >= 5) {
+        winner.textContent = `Computer wins! Total rounds played: ${roundNum}`
+    }
+}
+
+
+// Event listener to restart the total scores and start from 0 - 0
+reset.addEventListener('click', resetGame);
+
+// Function for computer to make a random selection
+function getComputerChoice() {
+
+    const random = Math.floor(Math.random() * compChoices.length);
+
+    return compChoices[random];
+}
+
+// Function to play a round of RPS. 
 function playRound(playerSelection, computerSelection) {
 
     computerSelection = computerSelection.toLowerCase();
@@ -38,15 +116,24 @@ function playRound(playerSelection, computerSelection) {
         || (playerSelection === `scissors` && computerSelection === `paper`)
         || (playerSelection === `paper` && computerSelection === `rock`)) {
 
-        return `Player wins! ${capitalizeFirst(playerSelection)} beats ${capitalizeFirst(computerSelection)}`;
+        playScore += 1;
+        roundNum++;
+        playerDisplay.textContent = `Player score: ${playScore}`;
+
+        return `Round: ${roundNum} - Player wins! ${capitalizeFirst(playerSelection)} beats ${capitalizeFirst(computerSelection)} - Score so far - Player: ${playScore}, Computer: ${compScore}`;
 
     } else if (playerSelection === computerSelection) {
 
-        return `Tie Game!`;
+        roundNum++;
+        return `Round: ${roundNum} - Tie Game! - Score so far - Player: ${playScore}, Computer: ${compScore}`;
 
     } else {
 
-        return `Player loses! ${capitalizeFirst(computerSelection)} beats ${capitalizeFirst(playerSelection)}`;
+        roundNum++;
+        compScore += 1;
+        compDisplay.textContent = `Computer score: ${compScore}`;
+
+        return `Round: ${roundNum} - Player loses! ${capitalizeFirst(computerSelection)} beats ${capitalizeFirst(playerSelection)}- Score so far - Player: ${playScore}, Computer: ${compScore}`;
     }
 }
 
@@ -57,13 +144,6 @@ function capitalizeFirst(word) {
     return first + word.slice(1);
 }
 
-// //Create a function called game()
-// function game() {
 
-//     for (let i = 1; i <= 5; i++) {
-//         const playerSelection = prompt(`Select Rock, Paper, Scissors`).toLowerCase();
-//         console.log(playRound(playerSelection, getComputerChoice()));
-//     }
-// }
 
 
